874d673c8df396ae2f36f54456dfdff5ff6cc526
Sinu5oid <adm-insert@ya.ru>
2017-07-05 15:39:22 +0600

    Performed code cleanup

--- app/src/main/java/ru/omgtu/calculator/MainActivity.java
+++ app/src/main/java/ru/omgtu/calculator/MainActivity.java
@@ -9,33 +9,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
-    Button button5;
-    Button button6;
-    Button button7;
-    Button button8;
-    Button button9;
-    Button button0;
-    Button buttonPlus;
-    Button buttonMinus;
-    Button buttonMultiply;
-    Button buttonDivide;
-    Button buttonSqrt;
-    Button buttonOP;
-    Button buttonCP;
-    Button buttonClear;
-    Button buttonDot;
-    Button buttonEqual;
-    Button butonBS;
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0,
+            buttonPlus, buttonMinus, buttonMultiply, buttonDivide, buttonSqrt, buttonOP, buttonCP,
+            buttonClear, buttonDot, buttonEqual, buttonBS;
     TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findComponentViewsById();
+        setComponentListeners();
+    }
+
+    void findComponentViewsById(){
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -56,9 +43,10 @@ public class MainActivity extends AppCompatActivity {
         buttonClear = (Button) findViewById(R.id.buttonClear);
         buttonDot = (Button) findViewById(R.id.buttonDot);
         buttonEqual = (Button) findViewById(R.id.buttonEqual);
-        butonBS = (Button) findViewById(R.id.buttonBS);
+        buttonBS = (Button) findViewById(R.id.buttonBS);
         tvResult = (AutoCompleteTextView) findViewById(R.id.result);
-
+    }
+    void setComponentListeners(){
         View.OnClickListener oclButton0 = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -206,9 +194,7 @@ public class MainActivity extends AppCompatActivity {
         buttonCP.setOnClickListener(oclButtonCP);
         buttonClear.setOnClickListener(oclButtonClear);
         buttonEqual.setOnClickListener(oclButtonEqual);
-        butonBS. setOnClickListener(oclButtonBS);
-
-
+        buttonBS. setOnClickListener(oclButtonBS);
     }
 
 }
