a8fdb9720f045a26fc068081590d752c7d6342b7
Sinu5oid <adm-insert@ya.ru>
2017-07-06 13:33:44 +0600

    * Added a translation * Added a new activity * Color rearrangement

--- app/src/main/java/ru/omgtu/calculator/Login.java
+++ app/src/main/java/ru/omgtu/calculator/Login.java
@@ -12,25 +12,36 @@ public class Login extends AppCompatActivity {
     Intent intent;
     TextView tvLogin;
     TextView tvPassword;
+    TextView tvLoginHint;
     Button buttonOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        buttonGoBack = (Button) findViewById(R.id.buttonIntent2);
         intent = new Intent(Login.this, MainActivity.class);
+        buttonGoBack = (Button) findViewById(R.id.buttonIntent2);
         buttonOk = (Button) findViewById(R.id.button);
         tvLogin = (TextView) findViewById(R.id.login);
         tvPassword = (TextView) findViewById(R.id.password);
-
+        tvLoginHint = (TextView) findViewById(R.id.textLoginHint);
+        View.OnClickListener oclButtonOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvLoginHint.setText(getResources().getString(R.string.hintLogin) + ": "
+                        + tvLogin.getText() + " | " +
+                        getResources().getString(R.string.hintPassword) + ": " +
+                        tvPassword.getText());
+            }
+        };
         View.OnClickListener oclButtonGoBack = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
             }
         };
+        buttonOk.setOnClickListener(oclButtonOk);
         buttonGoBack.setOnClickListener(oclButtonGoBack);
+
     }
 }
